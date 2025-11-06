7ba44cf8b104efb619b5dae3077d3ca48b084714
rdprince08 <prince.raj.080870@gmail.com>
2025-11-06 20:14:31 +0530

    today update

---
+++ src/data/EmployeeDTo.java
@@ -0,0 +1,77 @@
+package data;
+
+public class EmployeeDTo {
+    private Integer employeeId;
+    private String name;
+    private String job;
+    private Integer manager;
+    private String hireDate;
+    private Integer salary;
+    private Integer commission;
+//    private Integer deptNo;
+
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Integer getManager() {
+        return manager;
+    }
+
+    public void setManager(Integer manager) {
+        this.manager = manager;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getCommission() {
+        return commission;
+    }
+
+    public void setCommission(Integer commission) {
+        this.commission = commission;
+    }
+
+//    public Integer getDeptNo() {
+//        return deptNo;
+//    }
+
+//    public void setDeptNo(Integer deptNo) {
+//        this.deptNo = deptNo;
+//    }
+}
--- src/services/EmployeeServiceImpl.java
+++ src/services/EmployeeServiceImpl.java
@@ -1,5 +1,9 @@
 package services;
 
+import data.EmployeeDAO;
+import data.EmployeeDTo;
+import util.EmployeeValidator;
+
 import java.util.Scanner;
 
 public class EmployeeServiceImpl implements EmployeeService{
@@ -7,21 +11,38 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public void addEmployee() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Employee ID");
-        Integer employeeID = sc.nextInt();
+//        System.out.println("Enter Employee ID");
+//        Integer employeeId = sc.nextInt();
         System.out.println("Enter employee name");
         String name = sc.next();
         System.out.println("Enter employee job");
         String job = sc.next();
         System.out.println("Enter employee manager");
-        String manager = sc.next();
+        Integer manager = sc.nextInt();
         System.out.println("Enter employee hireDate");
         String hireDate = sc.next();
         System.out.println("Enter employee salary");
         Integer salary = sc.nextInt();
         System.out.println("Enter employee commission");
         Integer commission = sc.nextInt();
-        System.out.println("Enter employee department number");
-        Integer department = sc.nextInt();
+//        System.out.println("Enter employee department number");
+//        Integer deptNo = sc.nextInt();
+
+        EmployeeDTo empDto = new EmployeeDTo();
+//        empDto.setEmployeeId(employeeId);
+        empDto.setName(name);
+        empDto.setJob(job);
+        empDto.setManager(manager);
+        empDto.setHireDate(hireDate);
+        empDto.setSalary(salary);
+        empDto.setCommission(commission);
+//        empDto.setDeptNo(deptNo);
+
+        System.out.println("Trying to save employee data");
+
+        EmployeeValidator.validator(empDto);
+        EmployeeDAO empDAo = new EmployeeDAO();
+        empDAo.save(empDto);
+        System.out.println("Employee data saved!!");
     }
 }
